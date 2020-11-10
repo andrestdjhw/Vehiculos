f0fa2b97b0043866600142dc315fe1dfd4fb4933
Daniel Andres <guerrerohtrujillo@gmail.com>
2020-11-09 19:21:03 -0600

    Apis Corriendo con DB MySQL

--- routes/api2/vehiculo.js
+++ routes/api2/vehiculo.js
@@ -12,7 +12,7 @@ var con = mysql.createPool({
     multipleStatements: true
 
 });
- 
+//Muestra Todos Los Vehiculos Existentes en la Base de Datos 
 router.get('/get_vehiculos', (req, res, next)=>{
     var query= 'select * from vehiculos';
     con.query(query, (err, result, field)=>{
@@ -23,5 +23,46 @@ router.get('/get_vehiculos', (req, res, next)=>{
         }
     });
 });
+
+//Devuelve un Vehiculo consultado por ID
+router.get('/get_vehiculo', (req, res, next)=>{
+    var query= 'select * from vehiculos where id = ?';
+    var values= [req.query.id];
+    con.query(query, values,  (err, result, field)=>{
+        if(err){
+            next(err);
+        }else{
+            res.status(200).json(result); 
+        }
+    });
+});
+
+//Insertar un nuevo vehiculo
+router.post('/insert_vehiculo', (req, res, next)=>{
+    var query= 'INSERT INTO `prueba`.`vehiculos` (`propietario`, `color`, `marca`, `modelo`) VALUES (?, ?, ?, ?)';
+    var values= [req.body.id, req.body.propietario,
+                req.body.color, req.body.marca, req.body.modelo];
+    con.query(query, values,  (err, result, field)=>{
+        if(err){
+            next(err);
+        }else{
+            res.status(200).json(result); 
+        }
+    });
+});
+
+//Eliminar un Vehiculo
+router.delete('/delete_vehiculo', (req, res, next)=>{
+    var query= 'delete from vehiculos where id = ?';
+    var values= [req.query.id];
+    con.query(query, values,  (err, result, field)=>{
+        if(err){
+            next(err);
+        }else{
+            res.status(200).json(result); 
+        }
+    });
+});
+
 
 module.exports= router;
